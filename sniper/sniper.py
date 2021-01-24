7480bf0a871ee28695e81bb92a4cbdf4f5453a31
h0nda <41300123+davidbaszucki@users.noreply.github.com>
2021-01-24 14:27:39 +0100

    Update sniper.py

--- sniper/sniper.py
+++ sniper/sniper.py
@@ -15,16 +15,14 @@ target = None
 target_updated = 0
 target_lock = threading.Lock()
 
-PRODUCT_ID_RE = re.compile(r'data\-product\-id="(\d+)"')
-PRICE_RE = re.compile(r'data\-expected\-price="(\d+)"')
-SELLER_ID_RE = re.compile(r'data\-expected\-seller-id="(\d+)"')
-USERASSET_ID_RE = re.compile(r'data\-lowest\-private\-sale\-userasset\-id="(\d+)"')
+BULK_RE = re.compile(r'data\-product\-id="(\d+)".+?data\-expected\-price="(\d+)".+?data\-expected\-seller-id="(\d+)".+?data\-lowest\-private\-sale\-userasset\-id="(\d+)"', re.DOTALL)
 
 def parse_item_page(data):
-    product_id = int(PRODUCT_ID_RE.search(data).group(1))
-    price = int(PRICE_RE.search(data).group(1))
-    seller_id = int(SELLER_ID_RE.search(data).group(1))
-    userasset_id = int(USERASSET_ID_RE.search(data).group(1))
+    match = BULK_RE.search(data)
+    product_id = int(match.group(1))
+    price = int(match.group(2))
+    seller_id = int(match.group(3))
+    userasset_id = int(match.group(4))
     return product_id, price, seller_id, userasset_id
 
 # load cookie
